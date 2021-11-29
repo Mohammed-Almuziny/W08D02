8c241a10f354c787bc2874d74cb9df2b9920e395
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-29 19:37:23 +0300

    move roles controller to ther file

--- routers/routes/roles.js
+++ routers/routes/roles.js
@@ -1,36 +1,10 @@
 const express = require("express");
 
-const rolesModel = require("./../../db/models/roles");
+const { createRole, getRole } = require("./../controllers/roles");
 
 const rolesRouter = express.Router();
 
-rolesRouter.post("/createRole", (req, res) => {
-  const { role, permissions } = req.body;
-
-  const newRole = new rolesModel({
-    role,
-    permissions,
-  });
-
-  newRole
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-rolesRouter.get("/getRole", (req, res) => {
-  rolesModel
-    .find({})
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+rolesRouter.post("/createRole", createRole);
+rolesRouter.get("/getRole", getRole);
 
 module.exports = rolesRouter;
