6f8ca586d16b2478df40882cbb9965b817ca4e16
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-29 22:12:28 +0300

    hash and dehash the password

--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -1,13 +1,19 @@
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
 const usersModel = require("./../../db/models/users");
 
-const register = (req, res) => {
+const SALT = Number(process.env.SALT);
+
+const register = async (req, res) => {
   const { email, password, role } = req.body;
 
   const savedEmail = email.toLowerCase();
+  const savedPassword = await bcrypt.hash(password, SALT);
 
   const newUser = new usersModel({
     email: savedEmail,
-    password,
+    password: savedPassword,
     role,
   });
 
@@ -19,19 +25,21 @@ const register = (req, res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
-
-  res.status(200).json(req.body);
 };
 
 const logIn = (req, res) => {
   const { email, password } = req.body;
 
+  const savedEmail = email.toLowerCase();
+
   usersModel
-    .findOne({ email })
-    .then((result) => {
+    .findOne({ email: savedEmail })
+    .then(async (result) => {
       if (result) {
-        if (result.email === email) {
-          if (result.password === password) {
+        if (result.email === savedEmail) {
+          const savedPassword = await bcrypt.compare(password, result.password);
+
+          if (savedPassword) {
             res.status(200).json(result);
           } else {
             res.status(400).json("invalid email or password");
